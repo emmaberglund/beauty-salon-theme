6a4eaa81e6cafc3d093d62950b71aedbfb9d78f3
Emma Berglund <emma.berglund@live.se>
2016-02-12 15:15:10 +0100

    Instagram link

--- front-page.php
+++ front-page.php
@@ -24,7 +24,8 @@
 
     <div class="row">
         <div class="twelve columns introduction">
-            <img src="images/instagram-icon-green.png">
+
+            <img src="<?php echo get_template_directory_uri(); ?>/images/instagram-icon-green.png">
             <?php dynamic_sidebar('Instagram'); ?>
         </div>
     </div>
