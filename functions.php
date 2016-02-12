e1a682567509c607ff0fa5cb0af2e850a8872428
Emma Berglund <emma.berglund@live.se>
2016-02-12 14:46:37 +0100

    Mobile menu with CSS transition

--- functions.php
+++ functions.php
@@ -9,6 +9,17 @@ function wpt_theme_styles(){
 add_action('wp_enqueue_scripts', 'wpt_theme_styles' );
 
 
+function add_my_script() {
+    wp_enqueue_script(
+        'script', // name your script so that you can attach other scripts and de-register, etc.
+        get_template_directory_uri() . '/js/script.js', // this is the location of your script file
+        array('jquery') // this array lists the scripts upon which your script depends
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'add_my_script' );
+
+
 function our_widgets_init(){
     register_sidebar([
         'name' => 'Sidebar',
