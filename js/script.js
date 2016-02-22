7f1718a4eec3d871d67dd33d3394777585b4d6f7
Emma Berglund <emma.berglund@live.se>
2016-02-22 14:41:53 +0100

    Header changes color on scroll

--- js/script.js
+++ js/script.js
@@ -4,5 +4,25 @@ jQuery(function($) { // DOM is now read and ready to be manipulated
         console.log( "ready!" );
     });
 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() == 0) {
+            console.log("hej");
+            $('#header').css({
+                    'background-color': 'black',
+                    'box-shadow': 'none',
+                    '-moz-box-shadow' : 'none',
+                    '-webkit-box-shadow' : 'none' });
+        }
+        else {
+            console.log("hejdå");
+            $('#header').css({
+                    'background-color': 'rgba(0, 0, 0, 0.66)',
+                    //'box-shadow': '0px 10px 10px #888',
+                    //'-moz-box-shadow' : '0px 10px 10px #888',
+                    //'-webkit-box-shadow' : '0px 10px 10px #888'
+                });
+        }
+    });
+
 
 });
