512d63d6eaa235a181d4db325c724f834cb8f960
Camilla Borg <camillaborg88@gmail.com>
2016-02-22 15:26:56 +0100

    More work on Om Oss

--- about-template.php
+++ about-template.php
@@ -18,22 +18,21 @@ if(have_posts()) :
         <main>
             <div class="container">
                 <div class="row below-header">
+                    <div class="about-us">
                     <?php
                     $count = 1;
                     foreach ($pages as $page) {
                         ?>
-
-
-                        <div class="about-employee three columns">
-                            <div class ="about-employee-img" style="background-image:url(<?php echo wp_get_attachment_image_src(get_post_thumbnail_id($page->ID), 'medium')[0]; ?>);">
-                            </div>
-                            <h2><?php echo $page->post_title; ?></h2>
+                        <div class="three columns workers">
                             <p><?php echo $page->post_content; ?></p>
                         </div>
 
 
 
-                          <?php if($count % 3 == 0) {?>
+
+
+                          <?php if($count % 4 == 0) {?>
+                                </div>
                                 </div>
                                 <div class="row">
                           <?php }
